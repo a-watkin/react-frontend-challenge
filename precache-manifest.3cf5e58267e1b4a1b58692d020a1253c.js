af7a99c05d3d57a84d4d8c0db8176c538189cd86
a <awatkin.work@gmail.com>
2019-09-23 23:44:20 +0100

    Updates

---
+++ precache-manifest.3cf5e58267e1b4a1b58692d020a1253c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386eae3fe8b379e6b3f733b307865519",
+    "url": "/react-frontend-challenge/index.html"
+  },
+  {
+    "revision": "ac0e45907bfd6872d452",
+    "url": "/react-frontend-challenge/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c64c2a7059d6da30e394",
+    "url": "/react-frontend-challenge/static/css/main.58ae495c.chunk.css"
+  },
+  {
+    "revision": "ac0e45907bfd6872d452",
+    "url": "/react-frontend-challenge/static/js/2.02165781.chunk.js"
+  },
+  {
+    "revision": "c64c2a7059d6da30e394",
+    "url": "/react-frontend-challenge/static/js/main.0453323c.chunk.js"
+  },
+  {
+    "revision": "8ff6afd70d1b619859f8",
+    "url": "/react-frontend-challenge/static/js/runtime~main.1149f780.js"
+  },
+  {
+    "revision": "238d2dac79c347aee2865022772c2615",
+    "url": "/react-frontend-challenge/static/media/1.238d2dac.png"
+  },
+  {
+    "revision": "3a73a154a291d2e3b1bf6d45254c5a7a",
+    "url": "/react-frontend-challenge/static/media/2.3a73a154.png"
+  },
+  {
+    "revision": "735b9d5869a23b4fa3aead3fcfaee412",
+    "url": "/react-frontend-challenge/static/media/3.735b9d58.png"
+  },
+  {
+    "revision": "7d04245236807e36f372a28fdf548f91",
+    "url": "/react-frontend-challenge/static/media/4.7d042452.png"
+  }
+]);
